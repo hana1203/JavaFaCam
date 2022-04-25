f6be7f9e2baf36b904e7e06f0f24c011b54597fc
Hana <hanaindec@gmail.com>
2022-04-25 23:16:45 +0900

    learned exception class

--- Ch6JavaFunctions/src/exceptionhandling/FileExceptionHandling.java
+++ Ch6JavaFunctions/src/exceptionhandling/FileExceptionHandling.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class FileExceptionHandling {
     public static void main(String[] args) {
         //FileInputStream 이 하는일: file 에서 자료를 읽어들이는 역할하는 Stream. 자료처리하는 스트림이랑 다르고 입출력위한 스트림.
-//        FileInputStream fis = null; //3 위해서 주석처리
-//1.
+        FileInputStream fis = null; //3 위해서 주석처리
+////1.
 //        try {
 //            fis = new FileInputStream("a.txt"); //a.txt 파일 가장 상위 프로젝트 폴더인 JavaFaCam 안에 만들어야 작동되네..
 //            //컴파일러에 의해 프로그램시에 익셉션 핸들링해야되는 익셉션 툴.
@@ -33,38 +33,38 @@ public class FileExceptionHandling {
 //        System.out.println("end");
 
 //2.
-//        //여러단계에서 close() 시켜야 되는걸 try catch finally 로 가능하다
-//        //finally 가 하는일: 오픈된 리소스(사용중인 리소스) 해제.
-//        //try 구문 호출되면 finally 는 항상 무조건 호출된다. return 이 있어도 호출됨.
-//        try {
-//            fis = new FileInputStream("a.txt");
-//        } catch (FileNotFoundException e) {
-//            System.out.println(e);
-//            return; //return 해도 finally 는 불린다.
-//            //흠 왜 난 return 넣은거랑 뺀거랑 결과값이 똑같지
-//        } finally {
-//            if(fis!=null) // 열렸을때 close() 를 해야됨.
-//            {
-//                try {
-//                    fis.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }System.out.println("finally 항상 수행");
-//        }System.out.println("end 도 수행");
+        //여러단계에서 close() 시켜야 되는걸 try catch finally 로 가능하다
+        //finally 가 하는일: 오픈된 리소스(사용중인 리소스) 해제.
+        //try 구문 호출되면 finally 는 항상 무조건 호출된다. return 이 있어도 호출됨.
+        try {
+            fis = new FileInputStream("a.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return; //return 해도 finally 는 불린다.
+            //흠 왜 난 return 넣은거랑 뺀거랑 결과값이 똑같지
+        } finally {
+            if(fis!=null) // 열렸을때 close() 를 해야됨.
+            {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }System.out.println("finally 항상 수행");
+        }System.out.println("end 도 수행");
 
 //3.
         //Try with resources 문
         //자바 9부터 처음에 리소스를 try() 안에 선언
 
-        try(FileInputStream fis = new FileInputStream("a.txt")) {
-
-        } catch (FileNotFoundException e) { //file 없을때 나는 익셉션
-            e.printStackTrace();
-        } catch (IOException e) { //close 할때 나는 익셉션
-            e.printStackTrace();
-        }
-        System.out.println("end");
+//        try(FileInputStream fis = new FileInputStream("a.txt")) {
+//
+//        } catch (FileNotFoundException e) { //file 없을때 나는 익셉션
+//            e.printStackTrace();
+//        } catch (IOException e) { //close 할때 나는 익셉션
+//            e.printStackTrace();
+//        }
+//        System.out.println("end");
         //finally 쓸 필요없이 익셉션 처리됨.
         //자동으로 close 가 되는데 그 이유는 AutoClosable 이라는 인터페이스를 구현한다.
         //AutoClosable 라는 인터페이스를 구현하면 이 객체가 exception 났건 정상종료되었건
